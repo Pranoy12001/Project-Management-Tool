91dfdc73a096d473f8a7a8fb3e705129b6c9899e
Pranoy <pranoy.mbstu12001@gmail.com>
2021-04-22 16:03:26 +0600

    show errors in response

--- restpmtool/src/main/java/com/pranoyit/restpmtool/controller/ProjectController.java
+++ restpmtool/src/main/java/com/pranoyit/restpmtool/controller/ProjectController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,9 +26,14 @@ public class ProjectController {
     @PostMapping("/addProject")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Project Object is invalid", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError fieldError : result.getFieldErrors()) {
+                errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map>(errorMap, HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Project>(projectService.saveOrUpdateProject(project),
+        return new ResponseEntity<>(projectService.saveOrUpdateProject(project),
                 HttpStatus.CREATED);
     }
 }
