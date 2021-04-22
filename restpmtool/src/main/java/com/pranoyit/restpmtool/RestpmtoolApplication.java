3a6fe7867689cfda4cc3ad3b6663e1efc372dbdd
Pranoy <pranoy.mbstu12001@gmail.com>
2021-04-22 13:26:40 +0600

    setup project

---
+++ restpmtool/src/main/java/com/pranoyit/restpmtool/RestpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.pranoyit.restpmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestpmtoolApplication.class, args);
+	}
+
+}
