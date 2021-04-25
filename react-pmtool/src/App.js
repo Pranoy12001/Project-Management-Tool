f48494f86611e95df0944ab4491a2b83835cb195
Pranoy <pranoy.mbstu12001@gmail.com>
2021-04-25 18:27:11 +0600

    added redux store

--- react-pmtool/src/App.js
+++ react-pmtool/src/App.js
@@ -3,16 +3,20 @@ import Dashboard from './components/Dashboard';
 import Header from './components/layout/Header';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import AddProject from './components/project/AddProject';
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route path="/dashboard" component={Dashboard} exact></Route>
-        <Route path="/addProject" component={AddProject} exact></Route>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route path="/dashboard" component={Dashboard} exact></Route>
+          <Route path="/addProject" component={AddProject} exact></Route>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
