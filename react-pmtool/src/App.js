420a95891a9ec9c2d883215dcb2524b90b4cb52b
Pranoy <pranoy.mbstu12001@gmail.com>
2021-04-25 16:05:37 +0600

    updated components

--- react-pmtool/src/App.js
+++ react-pmtool/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/layout/Header';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import AddProject from './components/project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard></Dashboard>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/dashboard" component={Dashboard} exact></Route>
+        <Route path="/addProject" component={AddProject} exact></Route>
+      </div>
+    </Router>
   );
 }
 
--- react-pmtool/src/components/Dashboard.js
+++ react-pmtool/src/components/Dashboard.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import Header from './layout/Header';
 import ProjectItem from './project/ProjectItem'
 import "bootstrap/dist/css/bootstrap.min.css";
+import CreateProjectButton from './project/CreateProjectButton';
 
 class Dashboard extends Component {
     render() {
         return (
-            <div>
-                <Header />
-                <h1 className="alert alert-warning">Welcome to Dashboard</h1>
-                <ProjectItem />
+            <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <CreateProjectButton />
+                            <br />
+                            <hr />
+                            <ProjectItem />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
